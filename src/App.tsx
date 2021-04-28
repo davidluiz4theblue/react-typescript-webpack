bd206828de876836ea776079ebdd88e6c1de95ef
Ly Hoang <ly.hoang@kyanon.digital>
2021-04-29 02:16:10 +0700

    init webpack config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import React, { useState } from "react";
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  interface newObject {
+    name: string;
+    age: number;
+  }
+  const arr = [1, 2, 3, 4, 5];
+  const [items, setItems] = useState<Array<number>>(arr);
+  const user: newObject = {
+    name: "Chris",
+    age: 46,
+  };
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <ul>
+        {items.map((item)=><li>{item}</li>)}
+      </ul>
+    </>
+  );
 };
 
 export default App;
